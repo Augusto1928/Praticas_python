abd29ce5ad82d8a6444dda9929d5f343296013d4
Augusto1928 <161625464+Augusto1928@users.noreply.github.com>
2024-09-22 19:42:30 -0300

    Update gato.py

--- gato.py
+++ gato.py
@@ -1,1 +1,12 @@
-print("propfiiowropfuwOCWOICDIOCHWOCHW")
+
+#CRIANDO O GATO
+
+class gato():
+  def __init__(self, cor):
+    self.cor = cor
+
+  def como(self):
+    print(self.cor)
+
+nao = gato(branco)
+nao.como()
